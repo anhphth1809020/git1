0360de69b4b65495fa9240a779cb76685fe67a6d
hong_anh <anhphth1809020@fpt.edu.vn>
2018-09-24 18:19:42 +0700

    Lan dau thao tac voi git

---
+++ cmake-build-debug/CMakeFiles/git1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/git1.dir/main.c.obj"
+  "git1.pdb"
+  "git1.exe"
+  "git1.exe.manifest"
+  "libgit1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/git1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
